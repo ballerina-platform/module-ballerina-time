0d760645e37e78ddd0d44aae3ca03d5c7630eeda
Sahan Hemachandra <45299562+sahanHe@users.noreply.github.com>
2023-02-16 15:12:40 +0530

    Update error message and tests

--- native/src/main/java/io/ballerina/stdlib/time/nativeimpl/ExternMethods.java
+++ native/src/main/java/io/ballerina/stdlib/time/nativeimpl/ExternMethods.java
@@ -71,8 +71,8 @@ public class ExternMethods {
             return new Utc(utcTimeInstant).build();
         } catch (DateTimeException e) {
             return Utils.createError(Errors.FormatError,
-                    "Provided '" + str.getValue() + "' is not adhere to the expected RFC 3339 format " +
-                            "'2007-12-03T10:15:30.00Z' ");
+                    "The provided string '" + str.getValue() + "' does not adhere to the expected RFC 3339 format " +
+                            "'YYYY-MM-DDTHH:MM:SS.SSZ'. ");
         }
     }
 
